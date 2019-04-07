e6f27cd9d323dc0f797f2247470549e5f7adf320
marax97 <pa-wo97@o2.pl>
2019-04-07 23:22:13 +0200

    Dodano zroznicowane dane i usunieto duplikaty

--- src/test/java/lab2_1/BinarySearchTest_v1.java
+++ src/test/java/lab2_1/BinarySearchTest_v1.java
@@ -10,51 +10,53 @@ import edu.iis.mto.bsearch.SearchResult;
 
 public class BinarySearchTest_v1 {
 
+    public int tab[];
+
     @Test
     public void isInSequence() {
-        int[] tab = {1};
+        tab = new int[] {1, 2};
         SearchResult result = BinarySearch.search(1, tab);
         Assert.assertThat(result.getPosition(), is(1));
     }
 
     @Test
     public void isNotInSequence() {
-        int[] tab = {1};
+        tab = new int[] {1};
         SearchResult result = BinarySearch.search(2, tab);
         Assert.assertThat(result.getPosition(), is(-1));
     }
 
     @Test
     public void isFirstElementInSequence() {
-        int[] tab = {1, 2, 3, 4};
+        tab = new int[] {1, 8, 6, 2};
         SearchResult result = BinarySearch.search(1, tab);
         Assert.assertThat(result.getPosition(), is(1));
     }
 
     @Test
     public void isLastElementInSequence() {
-        int[] tab = {1, 2, 3, 4};
-        SearchResult result = BinarySearch.search(4, tab);
-        Assert.assertThat(result.getPosition(), is(4));
+        tab = new int[] {3, 6, 4};
+        SearchResult result = BinarySearch.search(6, tab);
+        Assert.assertThat(result.getPosition(), is(2));
     }
 
     @Test
     public void isMiddleElementInSequence() {
-        int[] tab = {1, 2, 3, 4, 5};
+        tab = new int[] {1, 8, 3, 2, 5};
         SearchResult result = BinarySearch.search(3, tab);
         Assert.assertThat(result.getPosition(), is(3));
     }
 
     @Test
     public void isFound() {
-        int[] tab = {1, 2, 3, 4, 5};
+        tab = new int[] {1, 2, 3, 4, 5, 6};
         SearchResult result = BinarySearch.search(2, tab);
         Assert.assertThat(result.isFound(), is(true));
     }
 
     @Test
     public void isNotFound() {
-        int[] tab = {1, 2, 3, 4, 5};
+        tab = new int[] {15};
         SearchResult result = BinarySearch.search(6, tab);
         Assert.assertThat(result.isFound(), is(false));
     }
